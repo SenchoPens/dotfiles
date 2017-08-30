a16c19aa53d1f10cf1a8b06b8d9325beb3bab040
Sencho Pens <senya@chekanov.net>
2017-08-30 16:04:11 +0300

    Change install script

--- .installer.d/fonts.sh
+++
@@ -1,8 +0,0 @@
-# Install Nerd Fonts
-cd tmp/
-git clone https://github.com/ryanoasis/nerd-fonts.git
-sh /tmp/nerd-fonts/install.sh
-
-# Make system-wide
-sudo ln -s $(readlink -f ~/.local/share/fonts/NerdFonts) /usr/share/fonts/
-
