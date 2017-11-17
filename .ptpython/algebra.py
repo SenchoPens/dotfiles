cf152aa54dfbdefb95f0f19473967e30fa8d2c02
Arseny Chekanov <senya@chekanov.net>
2017-11-18 00:57:46 +0300

    Add square equation solver

--- .ptpython/algebra.py
+++ .ptpython/algebra.py
@@ -1,3 +1,15 @@
 from sympy import *
+
+
 init_printing()
 
+x = Symbol('x', real=True)
+
+
+def sse(a, b, c):
+    '''Find solutions for a square equation'''
+    if b % 2 == 0:
+        print(f'D/4 = {(b / 2) ** 2 - a * c}')
+    else:
+        print(f'D = {b ** 2 - 4 * a * c}')
+    return solve(a * x ** 2 + b * x + c)
