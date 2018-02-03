61cb605f65f5e4f1c9594b638028b0743461da89
Arseny Chekanov <senya@chekanov.net>
2018-02-04 00:32:59 +0300

    Add tmp.zsh for temporary shell settings

---
+++ .zshrc.d/tmp.zsh
@@ -0,0 +1,15 @@
+# temporary settings
+
+# Hyperledger tutorial
+hash -d byfn=~/Projects/fabric-samples/first-network/
+export PATH=$PATH:~/Projects/fabric-samples/bin/
+
+berserk2kindle() {
+  kindle
+  cd ~/Papers
+  for v in $@; do
+    comic2kindle Berserk/BERSERK\ vol."$v"* -o BerserkMobiHQ
+    sudo cp BerserkMobiHQ/BERSERK\ vol.$v.mobi /mnt/documents/
+  done
+  ukindle
+} 
