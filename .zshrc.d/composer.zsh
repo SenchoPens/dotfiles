a98767e424976082a9e3e9289b346805a3b41c29
Arseny Chekanov <senya@chekanov.net>
2018-02-19 20:50:18 +0300

    Add Hyperledger Composer shell aliases and functions

---
+++ .zshrc.d/composer.zsh
@@ -0,0 +1,37 @@
+alias composer-compile='composer archive create --sourceType dir --sourceName .'
+
+composer-help() {
+  echo 'To start, you need to set $FABRIC_TOOLS to the path of the fabric-tools repo and $COMPOSER_NETWORK_NAME to the name of the network you are currently building. You can put that in your .profile.
+Functions:
+        fabric-start - first, if fabric has not started, run 
+        fabric-clean - if fabric has already started and runs different network
+        composer-compile - to compile .bna, run 
+        composer-install-runtime - to install .bna to running fabric node
+        composer-deploy - to run installed composer network with admin password $1 and .bna $2
+        composer-ping - to check if network is still running
+        '
+}
+
+fabric-start() {
+  sh "$FABRIC_TOOLS/startFabric.sh"
+  sh "$FABRIC_TOOLS/createPeerAdminCard.sh"
+}
+  
+fabric-clean() {
+  docker kill $(docker ps -q)
+  sh "$FABRIC_TOOLS/stopFabric.sh"
+  sh "$FABRIC_TOOLS/teardownFabric.sh"
+}
+
+composer-deploy() {
+  composer network start --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret $1 \
+                         --archiveFile $2 --file networkadmin.card
+}
+
+composer-install-runtime() {
+  composer runtime install --card PeerAdmin@hlfv1 --businessNetworkName $COMPOSER_NETWORK_NAME
+}
+
+composer-ping() {
+  composer network ping --card "admin@$COMPOSER_NETWORK_NAME"
+}
